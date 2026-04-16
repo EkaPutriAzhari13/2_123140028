9fed3f1d78573e151f8f8a39ea60edcc6d1cde30
EkaPutriAzhari13 <eka.123140028@student.itera.ac.id>
2026-04-16 22:25:12 +0700

    Delete NewsReaderApp directory

--- NewsReaderApp/app/src/main/java/com/example/newsreaderapp/domain/Article.kt
+++
@@ -1,9 +0,0 @@
-package com.example.newsreaderapp.domain
-
-data class Article(
-    val id: Int,
-    val title: String,
-    val description: String,
-    val imageUrl: String,
-    val url: String // Tambahkan field URL
-)
